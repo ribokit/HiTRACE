418fef39c3fe8153675a5477e058711ce283ca2d
nbisaria <nbisaria@dac0a9d6-b594-0410-bd98-f2f7f028047b>
2012-02-03 23:17:19 +0000

    Removed Titrationanalysis
    git-svn-id: https://simtk.org/svn/hitrace@123 dac0a9d6-b594-0410-bd98-f2f7f028047b

--- Scripts/TitrationAnalysis/one_two.m
+++
@@ -1,8 +0,0 @@
-function [f, p1_name, p2_name ] = one_two( conc, K1, K2 );
-
-p1_name = 'K1';
-p2_name = 'K2';
-
-f = [ ones(1,length(conc));   (conc/K1);   (conc/K2).*(conc/K1) ];
-[dummy, Z]  = meshgrid( ones(1,3), sum( f ) );
-f = f./Z';
