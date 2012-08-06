11aa237082258282b6211898097cbe164c1a4ad6
kprotoss <kprotoss@dac0a9d6-b594-0410-bd98-f2f7f028047b>
2012-08-06 13:03:13 +0000

    bug fixed when xsel's length exceeds area_pred's.
    git-svn-id: https://simtk.org/svn/hitrace@160 dac0a9d6-b594-0410-bd98-f2f7f028047b

--- Scripts/STEP5_ManualSequenceAssignment/mark_sequence.m
+++ Scripts/STEP5_ManualSequenceAssignment/mark_sequence.m
@@ -307,7 +307,7 @@ for i = xsel_to_plot
   SHOW_MARKS = 1;
   if SHOW_MARKS & ~isempty( marks );
     for j = 1:numlanes
-      if(area_pred(i,j) == 1)
+      if(i <= size(area_pred, 1) && area_pred(i,j) == 1)
         plot( j, xsel(i), 'ro' );
       end
     end
