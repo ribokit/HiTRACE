4f49a5ee2471bd89b4b13f56adc5fe36eccde9f4
rhiju <rhiju@dac0a9d6-b594-0410-bd98-f2f7f028047b>
2013-02-24 19:03:12 +0000

    mark_sequence does not take marks, mutpos as input anymore [may later change this to be backwards compatible]
    git-svn-id: https://simtk.org/svn/hitrace@193 dac0a9d6-b594-0410-bd98-f2f7f028047b

--- Scripts/STEP5_ManualSequenceAssignment/DEPRECATED/mark_sequence_OLD.m
+++ Scripts/STEP5_ManualSequenceAssignment/DEPRECATED/mark_sequence_OLD.m
@@ -1,6 +1,6 @@
-function [xsel,seqpos] = mark_sequence( image_x, xsel, sequence, ...
-					offset, primer_binding_site, ...
-					area_pred );
+function [xsel] = mark_sequence_OLD( image_x, xsel, sequence, ...
+				 JUST_PLOT_SEQUENCE, offset, period, ...
+				 marks,mutpos, area_pred, peak_spacing, USE_GUI);
 % MARK_SEQUENCE - Tool for rapid manual assignment of bands in electropherograms.
 %
 %  [xsel] = mark_sequence( image_x, xsel, sequence, ...
@@ -28,6 +28,7 @@ function [xsel,seqpos] = mark_sequence( image_x, xsel, sequence, ...
 % We should allow input of seqpos (perhaps deprecate 'period').
 %
 if ~exist('xsel');  xsel = []; end
+if ~exist('JUST_PLOT_SEQUENCE');  JUST_PLOT_SEQUENCE = 0; end
 if ~exist('offset');  offset = -999; end
 if ~exist('period');  period = 1; end
 if ~exist('marks');  marks = []; end
@@ -49,33 +50,28 @@ end
 
 contrast_factor = 40/ mean(mean(abs(image_x)));
 
-%  Probably should just remove this... not clear if JUST_PLOT_SEQUENCE flag is needed anymore...
-%if (JUST_PLOT_SEQUENCE )
-%  if isstruct(USE_GUI); axes(USE_GUI.displayComponents); end;
-%  make_plot( image_x, xsel, ymin, ymax, sequence, JUST_PLOT_SEQUENCE, ...
-%	     contrast_factor, offset, period,marks,mutpos, area_pred);
-%  return;
-%end
+if (JUST_PLOT_SEQUENCE )
+  if isstruct(USE_GUI); axes(USE_GUI.displayComponents); end;
+  make_plot( image_x, xsel, ymin, ymax, sequence, JUST_PLOT_SEQUENCE, ...
+	     contrast_factor, offset, period,marks,mutpos, area_pred);
+  return;
+end
 
 numlanes = size(image_x,2);
 
 stop_sel = 0;
 
-% not clear if USE_GUI is needed anymore
-%if isstruct(USE_GUI)
-%  axes(USE_GUI.displayComponents);
-%  make_plot( image_x, xsel, ymin, ymax, sequence, JUST_PLOT_SEQUENCE, ...
-%	     contrast_factor, offset, period,marks,mutpos, area_pred);
-%  uiwait( msgbox( 'Are you ready to interactively annotate the sequence?','Ready?','modal' ) )
-%end
+if isstruct(USE_GUI)
+  axes(USE_GUI.displayComponents);
+  make_plot( image_x, xsel, ymin, ymax, sequence, JUST_PLOT_SEQUENCE, ...
+	     contrast_factor, offset, period,marks,mutpos, area_pred);
+  uiwait( msgbox( 'Are you ready to interactively annotate the sequence?','Ready?','modal' ) )
+end
 
 while ~stop_sel
-
-  % not clear if USE_GUI is needed anymore
-  %if isstruct(USE_GUI); axes(USE_GUI.displayComponents);; end;
-
-  make_plot( image_x, contrast_factor, ymin, ymax, xsel, ...
-	     sequence, offset, primer_binding_site, area_pred );
+  if isstruct(USE_GUI); axes(USE_GUI.displayComponents);; end;
+  make_plot( image_x, xsel, ymin, ymax, sequence, JUST_PLOT_SEQUENCE, ...
+	     contrast_factor, offset, period,marks,mutpos, area_pred);
   
   title( ['j,l -- zoom. i,k -- up/down. q -- quit. left-click -- select. \newline',...
 	  'x -- auto-assign. ',...
@@ -110,29 +106,37 @@ while ~stop_sel
             xsel = sort( xsel );
         end
    case {'q','Q','z','Z'}
-    stop_sel = 1;
     if( length(xsel) > 0 && (length(xsel) < length(sequence) || length(xsel) > length(sequence)) )
-      %if isstruct(USE_GUI)
-	%btn = questdlg(sprintf('Warning: You have assigned only %d band position(s), which is less/more than the total number of bands (%d). How do you want to proceed?', length(xsel), length(sequence)), ...
-	%	       'Warning!', 'Return to image', 'Force to go','Force to go');
-        %if(~strcmp(btn, 'Force to go'))
-	%  stop_sel = 0;
-        %end
-      %else
-	%fprintf('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
-	%fprintf('Warning: You have assigned only %d band position(s), which is less than the total number of bands (%d).\n', length(xsel), length(sequence));
-	%fprintf('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
-      %end
+      if isstruct(USE_GUI)
+	btn = questdlg(sprintf('Warning: You have assigned only %d band position(s), which is less/more than the total number of bands (%d). How do you want to proceed?', length(xsel), length(sequence)), ...
+		       'Warning!', 'Return to image', 'Force to go','Force to go');
+        if(strcmp(btn, 'Force to go'))
+	  stop_sel = 1;
+        end
+      else
+	fprintf('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
+	fprintf('Warning: You have assigned only %d band position(s), which is less than the total number of bands (%d).\n', length(xsel), length(sequence));
+	fprintf('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n')
+	stop_sel = 1;
+      end
+    else
+      stop_sel = 1;
     end
    case {'e','E'}
+    %hold on
+    %plot( yselpick, xselpick, 'go','markersize',12 );
+    %hold off
+    %pause;
     xsel = remove_pick( xsel, xselpick );
    case {'j','J'}
     current_relative_pos =  (xselpick - ymin)/(ymax-ymin);
+    %current_relative_pos = 0.5;
     yscale = (ymax - ymin)*0.75;
     ymin = xselpick - yscale * (current_relative_pos);
     ymax = xselpick + yscale * ( 1- current_relative_pos);
    case {'l','L'}
     current_relative_pos =  (xselpick - ymin)/(ymax-ymin);
+    %current_relative_pos = 0.5;
     yscale = (ymax - ymin)/0.75;
     ymin = xselpick - yscale * (current_relative_pos);
     ymax = xselpick + yscale * ( 1- current_relative_pos);
@@ -144,11 +148,18 @@ while ~stop_sel
     yscale = (ymax - ymin);
     ymin = ymin + yscale*0.05;
     ymax = ymax + yscale*0.05;
+   case {'i','i'}
+    yscale = (ymax - ymin);
+    ymin = ymin - yscale*0.05;
+    ymax = ymax - yscale*0.05;
+   case {'k','k'}
+    yscale = (ymax - ymin);
+    ymin = ymin + yscale*0.05;
+    ymax = ymax + yscale*0.05;
    case {'1'}
     contrast_factor = contrast_factor * sqrt(2);
    case {'2'}
     contrast_factor = contrast_factor / sqrt(2);
-   % are a,d,w,s even in use? if not, remove.
    case {'a','A'}
     xsel = 1.005*(xsel-min(xsel))+min(xsel);
    case {'d','D'}
@@ -157,39 +168,47 @@ while ~stop_sel
     xsel = xsel - 5;
    case {'s','S'}
     xsel = xsel + 5;
-   % these are 'big' jumps. again not in use -- so remove?
-   case {'b', 'B'}
-    yscale = (ymax - ymin);
-    ymin = ymin + yscale;
-    ymax = ymax + yscale;
-   case {'t', 'T'}
-    yscale = (ymax - ymin);
-    ymin = ymin - yscale;
-    ymax = ymax - yscale;
+%    case {'v','V'}
+%     zoom(1.10)
+%    case {'b', 'B'}
+%     zoom(0.90)
+      case {'b', 'B'}
+        yscale = (ymax - ymin);
+        ymin = ymin + yscale;
+        ymax = ymax + yscale;
+      case {'t', 'T'}
+        yscale = (ymax - ymin);
+        ymin = ymin - yscale;
+        ymax = ymax - yscale;
    case {'r', 'R'}
     xsel = []; % reset
    case {'x','X'}
-    seqpos = length(sequence) - [1:length(xsel)] + 1 + offset;			
-
-    % um, a guess. Probably could think of a better one...
-    peak_spacing = size( image_x, 1 )/ length(  sequence );
+    if length( mutpos ) == size( image_x, 2 ) 
+      seqpos = length(sequence) - [1:length(xsel)] + 1 + offset;			
+      % um, a guess. Probably could think of a better one...
+      peak_spacing = size( image_x, 1 )/ length(  sequence );
       
-    if ~exist( 'area_pred' ) | isempty( area_pred )
-      fprintf( 'You need to input data_types or area_pred if you want to use auto-assign!\n' )
+      if ~exist( 'area_pred' ) | isempty( area_pred )
+	% I am thinking of getting rid of mutpos/marks entirely, and
+	% replacing with area_pred -- rhiju
+      else
+	input_bounds = [];
+	if length( xsel ) == 2; 
+	  input_bounds = xsel; 
+	  peak_spacing = ( input_bounds(2) - input_bounds(1) ) / length( sequence );
+	end;
+	xsel = auto_assign_sequence( image_x, sequence, seqpos, offset, area_pred, peak_spacing, input_bounds, 0 );
+      end
     else
-      input_bounds = [];
-      if length( xsel ) == 2; 
-	input_bounds = xsel; 
-	peak_spacing = ( input_bounds(2) - input_bounds(1) ) / length( sequence );
-      end;
-      xsel = auto_assign_sequence( image_x, sequence, seqpos, offset, area_pred, peak_spacing, input_bounds, 0 );
+      fprintf( 'the guidemark definitions have the %d entries, does not match %d lanes in the data.\n', length( mutpos ), size( image_x,2) );
     end
   end
   
+  
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if length( xsel ) > length( sequence )+1
+
+if length( xsel ) > length( sequence )
   fprintf( 'WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!\n');
   fprintf( ' Number of selected positions %d exceeds length of sequence %d!\n', length(xsel),length(sequence) );
   fprintf( 'WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!\n');
@@ -216,30 +235,34 @@ if length( xsel ) > 0
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function  make_plot( image_x, contrast_factor, ymin, ymax, ...
-		     xsel, sequence, offset, primer_binding_site, area_pred );
+function make_plot( image_x, xsel, ymin, ymax, sequence, ...
+		    JUST_PLOT_SEQUENCE, contrast_factor, offset, period,marks,mutpos,area_pred )
 
 numlanes = size( image_x, 2 ) ;
 
-% why not just set contrast_factor based on colormap?
-% might be much faster.
-image(contrast_factor * abs(image_x));
+%if ( ~JUST_PLOT_SEQUENCE )
+ image(contrast_factor * abs(image_x));
+%end
 
 xlim = get(gca,'xlim');
 xscale = abs(xlim(1) - xlim(2));
 axis( [ 0.5 numlanes+0.5 ymin ymax]);
 
+xwid = 1.0;
+if ( length( xsel ) > 0 )
+  xwid = mean( abs(xsel(1:end-1) - xsel(2:end)) );
+end
+
 hold on
 xsel_to_plot = find( xsel >= ymin & xsel <= ymax );
 
-SHOW_LINES = 1;
-
 for i = xsel_to_plot
   
+  SHOW_LINES = 1;
   if SHOW_LINES
-    %if (~JUST_PLOT_SEQUENCE )
-    h1 = plot( [0.5 numlanes+0.5 ], [xsel(i) xsel(i)], 'color',[1 0.5 0] ); 
-    %end
+    if (~JUST_PLOT_SEQUENCE )
+      h1 = plot( [0.5 numlanes+0.5 ], [xsel(i) xsel(i)], 'color',[1 0.5 0] ); 
+    end
   end
   
   SHOW_LABELS = 1;
@@ -252,10 +275,15 @@ for i = xsel_to_plot
     seqnum = length(sequence) - i + 1 + offset;
     if  ( ~(offset == -999) ) %& JUST_PLOT_SEQUENCE)
       txt_to_show = [seqchar,num2str( seqnum)];
+    %      if ( JUST_PLOT_SEQUENCE ) txt_to_show = num2str(seqnum); end;
     end    
     
     show_text = 1;
+    if ( period > 1 & mod(seqnum, period) ~= 0 )
+      show_text = 0;
+    end
     if (show_text )
+      %h2 = text( 0.5-0.05*xscale, xsel(i), txt_to_show );        
       h2 = text( 0.5, xsel(i), txt_to_show );        
       set(h2,'HorizontalAlignment','right');
     end
@@ -281,15 +309,15 @@ for i = xsel_to_plot
   end
   
   SHOW_MARKS = 1;
-  %if SHOW_MARKS & ~isempty( marks );
-  for j = 1:numlanes
-    if (i <= size(area_pred,1))
-      if (area_pred(i,j) == 1)	    
-	plot( j, xsel(i), 'ro' );
-      end
+  if SHOW_MARKS & ~isempty( marks );
+    for j = 1:numlanes
+        if(i <= size(area_pred,1))
+          if(area_pred(i,j) == 1)	    
+            plot( j, xsel(i), 'ro' );
+          end
+        end
     end
   end
-  %end
   
   if (exist( 'h1' ) )
     set(h1,'color',mycolor);
@@ -304,6 +332,8 @@ end
 axis off
 hold off;
 
+%if ~JUST_PLOT_SEQUENCE
 axis( [ 0.5 numlanes+0.5 ymin ymax]);
+%end
 
 %axis on
