e2b3b1ce20d9a1ef85ce2aba35d848a80564ed65
rhiju <rhiju@dac0a9d6-b594-0410-bd98-f2f7f028047b>
2013-02-27 05:21:30 +0000

    turning off leakage correction by default -- will re-activate later. Also, now allowing extraction of more than one signal channel
    git-svn-id: https://simtk.org/svn/hitrace@231 dac0a9d6-b594-0410-bd98-f2f7f028047b

--- Scripts/QuickLook/quick_look.m
+++ Scripts/QuickLook/quick_look.m
@@ -1,7 +1,7 @@
-function [d, da, ylim, labels] = quick_look( dirnames, ylim, trace_subset, signals_and_ref, dye_names, moreOptions )
+function [d, d_ref, ylimit, labels] = quick_look( dirnames, ylimit, trace_subset, signals_and_ref, dye_names, moreOptions )
 % QUICK_LOOK  basic read-in script for ABI sequencer files for HiTRACE analysis
 %
-%  [d, da, ylim, labels] = quick_look( dirnames, ylim, trace_subset, signals_and_ref, dye_names, moreOptions )
+%  [d, d_ref, ylimit, labels] = quick_look( dirnames, ylimit, trace_subset, signals_and_ref, dye_names, moreOptions )
 %
 %   quick_look() runs scripts to read in directories of .ab1 files from ABI 
 %    do initial data processing, and give feedback.
@@ -10,18 +10,25 @@ function [d, da, ylim, labels] = quick_look( dirnames, ylim, trace_subset, signa
 %
 %     dirnames = list of directories names  {'dir1','dir2',...'dirN'}
 %
-%  The rest of the inputs are optional.
+%  The rest of the inputs are optional -- specify [] if you want the default.
 %
-%     ylim = vector of two values -- the minimum and maximum time point to show (ymin and ymax) [default is "auto-select"]
+%     ylimit = vector of two values -- the minimum and maximum time point to show (ymin and ymax) [default is "auto-select"]
 %     trace_subset = subset of electropherograms to align and return (e.g., [1:8 12:16])
 %     signals_and_ref = which color channels have signal. The last entry is the color channel number with
 %                                the reference ladder co-loaded in all samples. (Default: [1 4], i.e. 1 has signal, 4 has reference. )
-%     dye_names  = Names of dyes for each color channel, used for leakage correction. Currently we assume 4 channels. Input this as a cell of strings.
-%                    (Default: {'FAM','ROX'}). 
-%                  Note: the number of dye names should correspond to the number of signals_and_ref. If dye names is given as {} or [],
-%                    no leakage correction will be applied.
+%     dye_names  = Names of dyes for each color channel. Input this as a cell of strings.  Example: {'FAM','ROX'}). 
+%                  Note: the number of dye names should correspond to the number of signals_and_ref. If dye names is given as {},
+%                    no leakage correction will be applied. Default [temporary]: no leakage correction.
 %     moreOptions= Any of the following to turn off data processing steps: {'noPlotStuff', 'noNormalize', 
 %             'noSmoothBaselineSubtract',  'noLeakageCorrection' 'noLocalAlign'}  [Default: run al processing steps]
+%
+%  Outputs:
+%
+%     d      = all signal traces, after baseline subtraction, normalization & alignment. If user asks for more than one channel, 
+%               traces for first channel requested are given first, then traces for second channel, etc.
+%     d_ref  = reference trace, after all of above. Should be aligned ladders.
+%     ylimit = [ymin,ymax] which were found by auto-selection (or input by user)
+%     labels = plot labels for each trace, extracted from ABI files.
 %
 % (C) Rhiju Das, 2009-2011, 2013
 %   Thanks to S. Denny & C. Cheng for input in multicolor applications
@@ -30,29 +37,30 @@ function [d, da, ylim, labels] = quick_look( dirnames, ylim, trace_subset, signa
 d = []; da = []; labels = {}; 
 
 % make backwards compatible...
-if exist( 'trace_subset', 'var' ) & length(trace_subset) == 1 & length( trace_subset ) == 1 & trace_subset > ylim
+if exist( 'trace_subset', 'var' ) & length(trace_subset) == 1 & length( trace_subset ) == 1 & trace_subset > ylimit
   % this may be the old-style quick-look
   help( 'quick_look' );
   fprintf( '\nWARNING! WARNING! Are you using the old style quick_look with ymin and ymax as separate arguments?\n')
   fprintf( '  Give them instead in a vector [ymin,ymax].\n Or consider not specifying them -- there  is an auto-range finder now! \n\n\n');
   fprintf( 'Press a key to continue\n' )
   pause
-  ylim = [ylim, trace_subset ]; trace_subset = [];  
+  ylimit = [ylimit, trace_subset ]; trace_subset = [];  
   if exist( 'signals_and_ref' ) trace_subset = signals_and_ref; end;
   signals_and_ref = [];
 end
 
-if ~exist( 'ylim', 'var'); ylim = []; end;
+if ~exist( 'ylimit', 'var'); ylimit = []; end;
 if ~exist( 'signals_and_ref', 'var' ) | isempty( signals_and_ref ); signals_and_ref = [1 4]; end;
 if length( signals_and_ref ) < 2; fprintf( 'Must have at least 2 channels specified in signals_and_ref!\n'); return; end;
 if ~exist( 'dye_names', 'var' )  | ~iscell( dye_names )
-  if length( signals_and_ref ) == 2; 
-    dye_names = {'FAM','ROX'};
-  elseif length( signals_and_ref ) == 3; 
-    dye_names = {'FAM','HEX','ROX'}; 
-  elseif length( signals_and_ref ) == 4; 
-    dye_names = {'FAM','HEX','TAMRA','ROX'}; 
-  end    
+  dye_names = {}; % signal to not apply a leakage correction -- later use FAM/ROX as default.
+  %if length( signals_and_ref ) == 2; 
+  %  dye_names = {'FAM','ROX'};
+  %elseif length( signals_and_ref ) == 3; 
+  %  dye_names = {'FAM','HEX','ROX'}; 
+  %elseif length( signals_and_ref ) == 4; 
+  %  dye_names = {'FAM','HEX','TAMRA','ROX'}; 
+  %end    
 end;
 if length( dye_names ) > 0 & length( signals_and_ref ) ~= length( dye_names) ; fprintf( 'Length of dye_names must be 0 or match length of signals_and_ref\n'); return; end;
 
@@ -62,7 +70,7 @@ if length( dye_names ) > 0 & length( signals_and_ref ) ~= length( dye_names) ; f
 % dye_names_full = { 'FAM', '', '', 'ROX'} carries that information concisely.
 dye_names_full = get_dye_names_full( dye_names, signals_and_ref );
 
-sigcol = signals_and_ref( 1 ); % will generalize this in a bit.
+sigcol = signals_and_ref( 1 : end-1 ); % will generalize this in a bit.
 refcol = signals_and_ref( end );
 fprintf( 'Assuming reference channel: %d\n', refcol );
 
@@ -118,15 +126,15 @@ if tag(end) == '/'; tag = tag(1:end-1); end;
 
 line_pos = [ 0 ];
 filepath = '';
-[ data_all, filenames_all, data_init, data_length ] = ...
+[ data_all, filenames_all, data_set_starts, data_length ] = ...
     read_abi_dirs( filepath, dirnames, dye_names_full, PLOT_STUFF );
 if ~exist( 'trace_subset' ) | length( trace_subset) == 0;  trace_subset = [ 1 : length( data_all ) ]; end
 
 if length( data_all ) == 0 ; return; end;
 
-line_pos = data_init-1;
+line_pos = data_set_starts - 1;
 numfiles = length( data_all );
-labels = filenames_all;
+filenames_all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % STAGE 2
@@ -147,23 +155,37 @@ d0_signal = [];
 d0_reference_ladder = [];
 
 
-for i = 1:length( trace_subset )  
-  if(size(data_all{trace_subset(i)},1) < max_size)
-    data_all{trace_subset(i)}(max_size, size(data_all{trace_subset(i)},2)) = 0;
-  end
+% If more than one color channel is specified, first fill a matrix with channel 1, then
+%  fill additional traces from channel 2, in the same order...
+count = 0;
+for m = 1:length(sigcol) % usually just channel 1.
+
+  for i = 1:length( trace_subset )  
+
     
-  % this is a straightforward subtraction of an offset.
-  d0_signal(1:size(data_all{trace_subset(i)}(:,1),1),i)           = baseline_subtract(data_all{trace_subset(i)}(:,sigcol));
-  d0_reference_ladder(1:size(data_all{trace_subset(i)}(:,1),1),i) = baseline_subtract(data_all{trace_subset(i)}(:,refcol));
+    L = size(data_all{trace_subset(i)},1);
+    if( L < max_size)
+      data_all{ trace_subset(i) }(max_size, size(data_all{trace_subset(i)},2)) = 0;
+    end
+    
+    % this is a straightforward subtraction of an offset.
+    count = count+1;
+    d0_signal          (1:L,count) = baseline_subtract(data_all{ trace_subset(i) }(:,sigcol(m)));
+    d0_reference_ladder(1:L,count) = baseline_subtract(data_all{ trace_subset(i) }(:,refcol));
+
+    labels{count} = filenames_all{i};
+  end
+
 end
 
-AUTOFIND_YLIM = 0;
-if isempty( ylim ) 
-  AUTOFIND_YLIM = 1;
+% If user has not  specified ymin,ymax in ylimit, figure it out.
+AUTOFIND_YLIMIT = 0;
+if isempty( ylimit ) 
+  AUTOFIND_YLIMIT = 1;
   [ymin, ymax] = findTimeRange( d0_signal );
-  ylim = [ymin,ymax];
+  ylimit = [ymin,ymax];
 end
-ymin = ylim(1); ymax = ylim(end);
+ymin = ylimit(1); ymax = ylimit(end);
 
 if PLOT_STUFF
   h = figure(2);
@@ -176,20 +198,20 @@ if PLOT_STUFF
   h=title( 'Signal (channel 1)');
   set( h,'interpreter','none' );
   
-  axis( [ 0.5 length( trace_subset )+0.5 ymin ymax] );
-  set( gca, 'xtick', 1:length( trace_subset ), ...
-	    'xticklabel', char( labels{trace_subset}  )  );
+  axis( [ 0.5 size( d0_signal, 2 )+0.5 ymin ymax] );
+  set( gca, 'xtick', 1:size( d0_signal,2 ), ...
+	    'xticklabel', char( labels )  );
   xticklabel_rotate;
-  make_dividers( trace_subset, line_pos, ymin, ymax );
+  make_dividers( d0_signal, line_pos, ymin, ymax );
 
   %figure(3)
   subplot(1,2,2);
   image( d0_reference_ladder*2);
   title( 'Reference ladder (channel 4)')
-  axis( [ 0.5 length( trace_subset )+0.5 ymin ymax] );
-  make_dividers( trace_subset, line_pos, ymin, ymax );
-    set( gca, 'xtick', 1:length(trace_subset), ...
-	    'xticklabel', char( labels{trace_subset}  )  );
+  axis( [ 0.5 size( d0_signal, 2 )+0.5 ymin ymax] );
+  make_dividers( d0_signal, line_pos, ymin, ymax );
+    set( gca, 'xtick', 1:size( d0_signal, 2), ...
+	    'xticklabel', char( labels  )  );
   xticklabel_rotate;
   
   colormap( 1- gray(100));
@@ -199,13 +221,12 @@ end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % STAGE 3
-
 reflane = 1; % this is a vestige of some old testing stuff.
 
-for i = 1:length(data_init)
-  start_index = data_init(i);
-  if i < length( data_init )
-    final_index = data_init(i+1)-1;
+for i = 1:length(data_set_starts)
+  start_index = data_set_starts(i);
+  if i < length( data_set_starts )
+    final_index = data_set_starts(i+1)-1;
   else
     final_index = length( data_all );
   end
@@ -218,30 +239,33 @@ data_align = align_capillaries_group( data_align_group, refcol, 1, 1);
 
 d = []; da= [];
 
-for i = 1:length( trace_subset); 
-  d(:,i)  = baseline_subtract(data_align{trace_subset(i)}(:,1));
-  da(:,i) = abs(baseline_subtract(data_align{trace_subset(i)}(:,refcol)));
+count = 0;
+for m = 1:length(sigcol) % usually just channel 1.
 
-  if(  ymax > size( d, 1 ) ) 
-    fprintf( 'WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!\n')
-    fprintf( 'WARNING! You specified a ymax (%5d) greater than trace length (%5d) WARNING!\n', ymax, size(d,1));
-    ymax = size( d, 1);
-    fprintf( 'WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!\n')
-  end
-
-  if NORMALIZE
-    %d(:,i) = d(:,i)/mean( data_align{trace_subset(i)}(1500:2500,3) );
-    d(:,i) =  d(:,i)/mean( abs(d(ymin:ymax,i)));
-    da(:,i) = da(:,i)/mean( abs(da(ymin:ymax,i)));
-  else
-    d(:,i) = d(:,i);
-    da(:,i) = da(:,i);
-  end
+  for i = 1:length( trace_subset); 
     
+    count = count+1;
+    d( :,count)  = baseline_subtract(data_align{trace_subset(i)}(:,sigcol(m)));
+    da(:,count) = abs(baseline_subtract(data_align{trace_subset(i)}(:,refcol)));
+    
+    if(  ymax > size( d, 1 ) ) 
+      fprintf( 'WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!\n')
+      fprintf( 'WARNING! You specified a ymax (%5d) greater than trace length (%5d) WARNING!\n', ymax, size(d,1));
+      ymax = size( d, 1);
+      fprintf( 'WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!\n')
+    end
+    
+    if NORMALIZE
+      %d(:,i) = d(:,i)/mean( data_align{trace_subset(i)}(1500:2500,3) );
+      d( :,count) =  d(:,count)/mean( abs(d(ymin:ymax,count)));
+      da(:,count) = da(:,count)/mean( abs(da(ymin:ymax,count)));
+    else
+      d( :,count) =  d(:,count);
+      da(:,count) = da(:,count);
+    end
+    
+  end
 end;
-
-%save d.mat d;
-%save da.mat da;
 
 if PLOT_STUFF
   h = figure(3);
@@ -251,12 +275,12 @@ if PLOT_STUFF
   
   %subplot(1,2,1);
   image( 50*d );
-  axis( [ 0.5 length( trace_subset )+0.5 ymin ymax] );
-  set( gca, 'xtick', 1:length( trace_subset ), ...
-	    'xticklabel', char( labels{ trace_subset}  )  );
+  axis( [ 0.5 size( d0_signal, 2 )+0.5 ymin ymax] );
+  set( gca, 'xtick', 1:size( d0_signal, 2 ), ...
+	    'xticklabel', char( labels  )  );
   xticklabel_rotate;
 
-  make_dividers( trace_subset, [], ymin, ymax );
+  make_dividers( d0_signal, [], ymin, ymax );
   h=title( [dirnames{1}]);
   set( h,'interpreter','none' )
   colormap(  1 - gray(100) )
@@ -269,9 +293,9 @@ if PLOT_STUFF
   %set(gcf, 'PaperPositionMode','auto','color','white');
   %%subplot(1,2,2);
   %image( 50*da );
-  %axis( [ 0.5 length( trace_subset )+0.5 ymin ymax] );
-  %set( gca, 'xtick', 1:length( trace_subset ), ...
-  %	    'xticklabel', char( labels{ trace_subset }  )  );
+  %axis( [ 0.5 size( d0_signal, 2 )+0.5 ymin ymax] );
+  %set( gca, 'xtick', 1:size( d0_signal, 2 ), ...
+  %	    'xticklabel', char( labels  )  );
   %xticklabel_rotate;
   %axis off
   %title( 'aligned reference ladders');
@@ -306,13 +330,13 @@ if PLOT_STUFF
   
   %subplot(1,2,1);
   image( 50*d );
-  axis( [ 0.5 length( trace_subset )+0.5 1 size(d,1)] );
-  %axis( [ 0.5 length( trace_subset )+0.5 ymin ymax] );
-  set( gca, 'xtick', 1:length( trace_subset ), ...
-	    'xticklabel', char( labels{ trace_subset}  )  );
+  axis( [ 0.5 size( d0_signal, 2 )+0.5 1 size(d,1)] );
+  %axis( [ 0.5 size( d0_signal, 2 )+0.5 ymin ymax] );
+  set( gca, 'xtick', 1:size( d0_signal, 2 ), ...
+	    'xticklabel', char( labels  )  );
   xticklabel_rotate;
 
-  make_dividers( trace_subset, [], 1, size( d, 1 ));
+  make_dividers( d0_signal, [], 1, size( d, 1 ));
   h=title( [dirnames{1}]);
   set( h,'interpreter','none' )
   colormap(  1 - gray(100) )
@@ -325,15 +349,15 @@ if PLOT_STUFF
   set(gcf, 'PaperPositionMode','auto','color','white');
   %subplot(1,2,2);
   image( 50*da );
-  axis( [ 0.5 length( trace_subset )+0.5 1 size(d,1)] );
-  %xlim( [ 0.5 length( trace_subset )+0.5 ] );
-  set( gca, 'xtick', 1:length( trace_subset ), ...
-	    'xticklabel', char( labels{ trace_subset }  )  );
+  axis( [ 0.5 size( d0_signal, 2 )+0.5 1 size(d,1)] );
+  %xlim( [ 0.5 size( d0_signal, 2 )+0.5 ] );
+  set( gca, 'xtick', 1:size( d0_signal, 2 ), ...
+	    'xticklabel', char( labels  )  );
   xticklabel_rotate;
   axis off
   title( 'aligned reference ladders');
 
-  make_dividers( trace_subset, [], 1, size( d, 1 ));
+  make_dividers( d0_signal, [], 1, size( d, 1 ));
   title( 'Reference ladder (channel 4)')
   
   colormap(  1 -gray(100) )
@@ -354,7 +378,7 @@ fprintf( '\n' );
 fprintf( 'ymin = %d\n', ymin)
 fprintf( 'ymax = %d\n\n', ymax)
 if length( dye_names_full ) > 0; fprintf( 'Applied leakage correction for color channels.\n' ); end;
-if AUTOFIND_YLIM;                fprintf( 'Used auto-find of ymin, ymax.\n' ); end;
+if AUTOFIND_YLIMIT;                fprintf( 'Used auto-find of ymin, ymax.\n' ); end;
 if NORMALIZE;                    fprintf( 'Normalized data based on mean peak intensity.\n' ); end;
 if SMOOTH_BASELINE_SUBTRACT;     fprintf( 'Applied subtration of smooth base line.\n' ); end;
 
@@ -365,7 +389,7 @@ labels = labels( trace_subset );
 function  make_dividers( trace_subset, line_pos, ymin, ymax );
 
 hold on
-for i = 1:length( trace_subset );
+for i = 1:size( trace_subset, 2 );
   plot( 0.5+i*[1 1], [ymin ymax],'k-', 'linew',0.25); 
 end
 for i = 2:length( line_pos );
