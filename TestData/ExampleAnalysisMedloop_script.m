e90ea2adcdd15cd0dee6879ae8369728807be2c4
rhiju <rhiju@dac0a9d6-b594-0410-bd98-f2f7f028047b>
2013-03-05 00:27:09 +0000

    update to get TestData script working
    git-svn-id: https://simtk.org/svn/hitrace@283 dac0a9d6-b594-0410-bd98-f2f7f028047b

--- TestData/ExampleAnalysisMedloop_script.m
+++ TestData/ExampleAnalysisMedloop_script.m
@@ -6,10 +6,9 @@
 % accurately infers the base pairs of a 35-nucleotide model RNA".
 % RNA 17:522-534.
 %
-% Updated in Jan. 2012, R. Das.
-% Updated again in Feb. 2013, R. Das to match HiTRACE v2.0
+% Updated in Feb. 2013, R. Das to match HiTRACE v2.0
 %
-% Check out more detailed run-through of the demo in:
+% A more detailed run-through of the demo will be made available soon in:
 %
 % https://docs.google.com/a/stanford.edu/Doc?docid=0AfZX7iE4SSSOZGM4MmpmbXZfMTI0cjJwMjJrZmg&hl=en
 %
@@ -28,7 +27,6 @@ filenames =  { ...
 
 d_align = quick_look( filenames );
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % OPTIONAL: align by piecewise-linear mapping (dynamic programming)
 align_blocks = [1:40]; % these are 'mutate/map columns'
@@ -36,47 +34,31 @@ d_align_before_more_alignment = d_align;
 d_align  = align_by_DP_fine( d_align_before_more_alignment, align_blocks );
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% OPTIONAL manual alignment -- kept here for completeness.
-%d_old = d_bsub_cut; anchorlines = [];
-%[d_manual_align, anchorlines] = align_userinput_saveanchorlines(100*d_old,1,1,anchorlines);
-%d_bsub_cut = d_manual_align;
-%d_align  = align_by_DP( d_bsub_cut, align_blocks );
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % definitions of what's in this data set.
 sequence = 'GGAACGACGAACCGAAAACCGAAGAAAUGAAGAAAGGUUUUCGGUACCGACCUGAAAACCAAAGAAACAACAACAACAAC';
-% the value you add to the sequence position to get the 'conventional numbering' (here the numbering used in the Medloop paper)
 structure= '..........((((((((((...............))))))))))...................................';
+% the value you add to the sequence position to get the 'conventional numbering' (here the numbering used in the Medloop paper)
 offset = -10;  
 first_RT_nucleotide = length( sequence ) - 20 + offset; % primer binds to last 20 nucleotides
 
-% What is in this data set?
-for i = 1:40; data_types{i} = 'DMS'; end;
-data_types(41:46) = { 'nomod','nomod','ddGTP','ddATP','ddCTP','ddTTP'};
-data_types(47:52) = { 'nomod','nomod','ddGTP','ddATP','ddCTP','ddTTP'};
+% definition of what to expect in each trace.
+for i = 1:40; data_types{i} = 'DMS'; end; % DMS mutate-and-map data.
+data_types(41:46) = { 'nomod','nomod','ddGTP','ddATP','ddCTP','ddTTP'}; % controls & sequence-ladders
+data_types(47:52) = { 'nomod','nomod','ddGTP','ddATP','ddCTP','ddTTP'}; % controls & sequence-ladders
 
 % option not taken here -- mark mutation positions instead.
 %mutpos = [ NaN 1:35, NaN 1 2 3];
 %for i = 1:length( mutpos );   data_types{i} = mutpos(i); end;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Interactive sequence annotation
+% Interactive sequence annotation. Can actually run auto-annotate --
+% select top (unextended) and bottom (extended) bands and hit 'x'.
 xsel = []; clf;
 [xsel,seqpos] = annotate_sequence( d_align, xsel, sequence, offset, data_types, first_RT_nucleotide, structure );
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Fit to Gaussians
-area_peak = do_the_fit_fast( d_align, xsel );
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Background subtraction and correction for 'overmodification', a.k.a., attenuation of longer products
-backgd_col = [41];
-norm_bins = [10 : (size(area_peak,1)-10) ];
-[area_bsub, darea_bsub]  = overmod_and_background_correct_logL( area_peak, backgd_col, norm_bins );
-
-image( area_bsub * 40)
-errorbar( area_bsub(:,1), darea_bsub(:,1) ); ylim([-0.5 2] )
-
+area_peak = fit_to_gaussians( d_align, xsel );
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Save as an RDAT file.
@@ -84,38 +66,24 @@ filename = 'ExampleAnalysisMedloop_NEW.rdat';
 name = 'Medloop';
 annotations = {'experimentType:MutateMap','chemical:Na-HEPES:50mM(pH8.0)','temperature:24C','modifier:DMS','processing:overmodificationCorrection','processing:backgroundSubtraction'};
 for i = 1:length(data_types); data_annotations{i} = { ['modifier:',data_types{i}] }; end;
+darea_peak = [];
 xsel_refine = [];
+mutpos = [];
 comments = {'Created with the commands in ExampleAnalysisMedloop_script.m'};
-	    
-output_workspace_to_rdat_file( filename, name, sequence, offset, seqpos, area_bsub, ...
-			       [], structure, ...
+
+output_workspace_to_rdat_file( filename, name, sequence, offset, seqpos, area_peak, ...
+			       mutpos, structure, ...
 			       annotations, data_annotations, ...
-			       darea_bsub, ...
+			       darea_peak, ...
 			       d_align, xsel, xsel_refine, comments );
 
 rdat = show_rdat( filename );
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Averaging across data sets.
-
-rdat  = show_rdat( 'ExampleAnalysisMedloop.rdat' );
-wt_mean = mean(rdat.reactivity(:,[1 37])');
-wt_err  = std(rdat.reactivity(:,[1 37])');
-
-rdat0 = show_rdat( 'ExampleAnalysisMedloop_previousDMSreplicate.rdat' );
-wt_mean0 = mean(rdat0.reactivity(:,[1 37])');
-wt_err0  = std(rdat0.reactivity(:,[1 37])');
-
-clf;
-errorbar( rdat.seqpos, wt_mean, wt_err ); hold on
-errorbar( rdat0.seqpos, wt_mean0, wt_err0, 'r'); 
-d_mean = {wt_mean, wt_mean0 };
-d_err = {wt_err, wt_err0 };
-[d_mean_final, d_err_final] = get_average_standard_state( d_mean, d_err );
-
-errorbar( rdat0.seqpos, d_mean_final, d_err_final, 'k'); hold off
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Create a VARNA figure
-easy_varna_fig( 'test_varna.html', sequence, structure, seqpos, offset, d_mean_final )
+DMS_data = boxplot_normalize( area_peak(:,1) );
+easy_varna_fig( 'test_varna.html', sequence, structure, seqpos, offset, DMS_data)
+
+% now open test_varna.html in firefox or other Java-enabled browser.
+
