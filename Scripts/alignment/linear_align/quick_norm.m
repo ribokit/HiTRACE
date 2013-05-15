fc284a7d546f8ee22ca59497f96c29c136b37c39
rhiju <rhiju@dac0a9d6-b594-0410-bd98-f2f7f028047b>
2013-05-15 14:49:44 +0000

    adding FORCE_POSITIVE option to quick_norm -- will disallow use of negative signal to flip sign during normalize. It is on by default now!
    git-svn-id: https://simtk.org/svn/hitrace@326 dac0a9d6-b594-0410-bd98-f2f7f028047b

--- Scripts/alignment/linear_align/quick_norm.m
+++ Scripts/alignment/linear_align/quick_norm.m
@@ -1,4 +1,4 @@
-function [d_out, norm_factor, d_out_err ] = quick_norm( d, bins, d_err )
+function [d_out, norm_factor, d_out_err ] = quick_norm( d, bins, d_err, FORCE_POSITIVE )
 % QUICK_NORM: normalize data based on mean in specified bins
 %
 % [d_out, norm_factor ] = quick_norm( d, bins );
@@ -9,6 +9,7 @@ function [d_out, norm_factor, d_out_err ] = quick_norm( d, bins, d_err )
 if nargin == 0;  help( mfilename ); return; end;
 
 if ~exist( 'd_err','var') d_err = 0*d; end;
+if ~exist( 'FORCE_POSITIVE','var') FORCE_POSITIVE = 1; end;
 
 do_transpose = 0;
 if size( d, 1) == 1 && size( d, 2)>1
@@ -27,8 +28,12 @@ end
 %end
 
 for i = 1:size(d,2)
-  norm_factor( i ) = 1.0 ./ mean( d( bins,i) );
-
+  if FORCE_POSITIVE
+    norm_factor( i ) = 1.0 ./ mean( max( d( bins,i), 0) );
+  else
+    norm_factor( i ) = 1.0 ./ mean( d( bins,i) );
+  end
+  
   if isnan( norm_factor(i) ) | isinf( norm_factor(i) ); norm_factor(i) = 0.0; end;
   
   d_out(:,i) = d(:,i) * norm_factor( i );
@@ -40,8 +45,6 @@ for i = 1:size(d,2)
   end
   
 end
-
-
 
 if do_transpose
   d_out = d_out';
