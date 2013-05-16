fe627202630fe0ca0491112ab59878f1ba09b865
rhiju <rhiju@dac0a9d6-b594-0410-bd98-f2f7f028047b>
2013-05-16 14:40:13 +0000

    a little code cleanup/simplification in auto_assign_sequence.m
    git-svn-id: https://simtk.org/svn/hitrace@328 dac0a9d6-b594-0410-bd98-f2f7f028047b

--- Scripts/sequence_annotation/auto_assign_sequence.m
+++ Scripts/sequence_annotation/auto_assign_sequence.m
@@ -2,26 +2,79 @@ function [xsel, D, msg] = auto_assign_sequence( image_x, sequence, seqpos, offse
 % AUTO_ASSIGN_SEQUENCE: (still experimental) automatic assignment of bands, given expected locations of marks
 %
 %
-% (C) R. Das, 2010-2011
-%
+% (C) R. Das, Hanjoo Kim, Sungroh Yoon, 2010-2011
+% (C) R. Das, 2013.
 
 if nargin == 0;  help( mfilename ); return; end;
 
 if ~exist('PLOT_STUFF','var'), PLOT_STUFF = 1; end
+if ~exist('ideal_spacing','var') | ~isempty(ideal_spacing)  | ideal_spacing == 0
+  ideal_spacing = guess_ideal_spacing( image_x );
+  if ( ideal_spacing == 0 );  ideal_spacing = floor(num_pixels / (size( area_pred, 1 )-1) );  end;
+end;
 if ~exist('input_bounds','var'), input_bounds = []; end
 if ~exist( 'data_types', 'var' ), data_types = []; end;
 
-[num_pixels, num_lanes] = size( image_x );
-nres = length( sequence );
-msg = [];
-exist_talepeak = true;
-
 FALSEPEAK_CUT = 8;
+[exist_talepeak, image_x, msg] = remove_tale_and_false_peaks( image_x, area_pred, FALSEPEAK_CUT );
+
+% note that this was from a time when we used to list sequence 
+% positions backward. Probably should fix this...
+s = area_pred;
+nres = length( sequence );
+s(1,:)    = 10;
+s(nres,:) = 1;
+sequence_at_bands = sequence( end:-1:1 );
+if exist_talepeak
+  s(nres+1,:) = 10;
+  sequence_at_bands = [sequence_at_bands, 'X'];
+end
+s( s == 0.0 ) = 0.01;
+
+tic
+[xsel, D] = solve_xsel_by_DP( image_x, s, sequence_at_bands, ideal_spacing, input_bounds, data_types );
+toc % read out time required for fit.
+
+if exist_talepeak
+  fprintf( 'Assigned fully extended band... one before nominal beginning of sequence.\n' );
+  %    xsel(end) = []; % keep this band!
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function ideal_spacing = guess_ideal_spacing( image_x )
+% look for closely spaced peaks using autocorrelation
+max_spacing = 50;
+possible_spacings = [];
+for i = 1:size( image_x, 2 )
+  spacings = localMaximum( autocorr( image_x(:,i), max_spacing ) );
+
+  % the first peak is always at 1 -- ignore that one and go to the next tightest spacing
+  if length( spacings ) > 1;
+    possible_spacings = [ possible_spacings, spacings(2) ];
+  end
+end
+
+if length( possible_spacings ) == 0
+  ideal_spacing = 0; % no clue.
+else
+  ideal_spacing = median( possible_spacings ); 
+end
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [exist_talepeak, image_x, msg] = remove_tale_and_false_peaks( image_x, area_pred, FALSEPEAK_CUT );
+
+exist_talepeak = true;
+msg = [];
+
+
+% getting rid of data at and beyond 'tale peaks' [fully extended band] which
+% can dominate fit.
+[num_pixels, num_lanes] = size( image_x );
 gee = zeros(1,num_lanes);
 spread = floor(num_pixels / (size( area_pred, 1 )-1) )/2;
-
-% getting rid of 'tale peaks' [fully extended band] which
-% can dominate fit.
 for i = 1:num_lanes
   % scale image
   scalefactor = mean( image_x(:,i) );
@@ -49,13 +102,7 @@ if (max(gee) < num_pixels)
     image_x(talepeakrange,:) = 0.0;
 end
 
-
-
-if ~exist('ideal_spacing','var') | ~isempty(ideal_spacing)  | ideal_spacing == 0
-  ideal_spacing = guess_ideal_spacing( image_x );
-  if ( ideal_spacing == 0 );  ideal_spacing = floor(num_pixels / (size( area_pred, 1 )-1) );  end;
-end;
-
+% Remove 'false peaks'
 n = num_lanes - 1;
 falsepeaks = find(image_x(peaks(1:tmp),n) > FALSEPEAK_CUT);
 falsepeaks((peaks(falsepeaks) < 0.25*num_pixels) | (peaks(falsepeaks) > 0.8*num_pixels)) = [];
@@ -68,57 +115,3 @@ if ~isempty(falsepeaks)
     msg = [msg 'detected false peak at (' num2str(min(falsepeakrange)) '~' num2str(max(falsepeakrange)) ') in baseline signal'];
     image_x(falsepeakrange,:) = 0;
 end
-
-if PLOT_STUFF
-  %after normalization
-  subplot(1,2,1);
-  image( image_x*50 ) 
-end
-
-s = area_pred;
-
-% note that this was from a time when we used to list sequence 
-% positions backward. Probably should fix this...
-s(1,:)    = 10;
-s(nres,:) = 1;
-sequence_at_bands = sequence( end:-1:1 );
-if exist_talepeak
-  s(nres+1,:) = 10;
-  sequence_at_bands = [sequence_at_bands, 'X'];
-end
-s( s == 0.0 ) = 0.01;
-
-if PLOT_STUFF
-  subplot(1,2,2);
-  image( s*50 );
-  colormap( 1 - gray(100) );
-end
-
-[xsel_fit, D] = solve_xsel_by_DP( image_x, s, sequence_at_bands, ideal_spacing, input_bounds, data_types );
-
-xsel = xsel_fit(1:end);
-if exist_talepeak
-  fprintf( 'Assigned fully extended band... one before nominal beginning of sequence.\n' );
-  %    xsel(end) = []; % keep this band!
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function ideal_spacing = guess_ideal_spacing( image_x )
-% look for closely spaced peaks using autocorrelation
-max_spacing = 50;
-possible_spacings = [];
-for i = 1:size( image_x, 2 )
-  spacings = localMaximum( autocorr( image_x(:,i), max_spacing ) );
-
-  % the first peak is always at 1 -- ignore that one and go to the next tightest spacing
-  if length( spacings ) > 1;
-    possible_spacings = [ possible_spacings, spacings(2) ];
-  end
-end
-
-if length( possible_spacings ) == 0
-  ideal_spacing = 0; % no clue.
-else
-  ideal_spacing = median( possible_spacings ); 
-end
-
