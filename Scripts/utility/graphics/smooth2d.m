e0ca1ea1f63ccfe63044abfaac0d299e99d98889
rhiju <rhiju@stanford.edu>
2013-10-02 15:01:59 -0700

    putting in smooth2d script

---
+++ Scripts/utility/graphics/smooth2d.m
@@ -0,0 +1,8 @@
+function x_smooth = smooth2d( x, niter );
+if ~exist( 'niter' )  niter = 2; end
+B = [ 0 0.1 0; 0.1 0.6 0.1; 0 0.1 0];
+x_smooth = conv2( x, B, 'same' );
+
+for  n = 1:niter-1;  
+  x_smooth = conv2( x_smooth, B, 'same' );
+end
