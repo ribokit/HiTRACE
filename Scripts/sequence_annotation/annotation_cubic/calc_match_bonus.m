b4e4e507f911cf49076a79a33b29b5546dfe5bd8
Seungmyung Lee <y2smile86@gmail.com>
2014-09-22 17:43:21 +0900

    recommit calc_match_bonus.m

--- Scripts/sequence_annotation/annotation_cubic/calc_match_bonus.m
+++ Scripts/sequence_annotation/annotation_cubic/calc_match_bonus.m
@@ -1,13 +1,14 @@
-function [ bonus ] = calc_match_bonus( peak_bonus, pos, prediction, prox_peak )
-% calculate peak matching bonus for non-primary lanes
+function [ bonus ] = calc_match_bonus( peak_bonus, pos, prediction, prox_peak, peak_range)
     bonus = 0;
     for lane = 1:size(prediction,2)-1
         if (prediction(lane) == 0)
             continue
         end
-        tmp = zeros(11, 1);
-        for i = -5:5
-            tmp(i + 6) = peak_bonus(pos + i, lane) * prox_peak(abs(i)+1);
+        tmp = zeros(peak_range*2+1, 1);
+        for i = -peak_range:peak_range
+            if (pos + i > 0)
+                tmp(i + peak_range + 1) = peak_bonus(pos + i, lane) * prox_peak(abs(i)+1);
+            end
         end
         bonus = bonus + max(tmp);
     end
@@ -16,3 +17,4 @@ function [ bonus ] = calc_match_bonus( peak_bonus, pos, prediction, prox_peak )
         bonus = bonus * (1/ratio)^0.5;
     end
 end
+
