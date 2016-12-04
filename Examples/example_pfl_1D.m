d6df48883187da99d03af49e6f57d02a85c5e2cb
T47 <t47@stanford.edu>
2016-12-03 17:03:59 -0800

    add comments to script example

--- Examples/example_pfl_1D.m
+++ Examples/example_pfl_1D.m
@@ -1,4 +1,6 @@
-% step 1
+%% STEP 1
+% make sure you are at the right path, i.e. the following folders are available
+% use command pwd to check where you are
 filenames = { ...
     'pfl_1D_QC&CM_2015-05-02_1802', ...
     'pfl_1D_QC&CM_2015-05-02_1803', ...
@@ -6,49 +8,65 @@ filenames = { ...
     'pfl_1D_QC&CM_2015-05-02_1805', ...
 };
 ylimit = [2501, 6000];
+% reorder is for reorganizing data lanes into MATLAB
 reorder = [1:8, 17:64, 9, 13, 10, 14, 11, 15, 12, 16];
 [d_align, d_ref_align, ylimit, labels] = quick_look(filenames, ylimit, reorder);
 
-% step 2
+%% STEP 2
+% the numbers in align_blocks correspond to columns in d_align
+% it has nothing to do with reorder
+% only align groups of similar nature, e.g. SHAPE
+% ddNTPs should never be included here
 align_blocks = {1:8, 9:24, 25:40, 41:56, 57:58, 59:60, 61:62, 63:64};
 d_align_before_more_alignment = d_align;
 d_align_dp_fine = align_by_DP_fine(d_align_before_more_alignment, align_blocks);
+% check for quality before accept
 d_align = d_align_dp_fine;
 
 
-% step 3
+%% STEP 3
 sequence = 'GGAGACCTCGAGTAGAGGTCAAAAGGGTCGTGACTGGCGAACAGGTGGGAAACCACCGGGGAGCGACCCCGGCATCGATAGCCGCCCGCCTGGGCAAACAACTCGAGTAGAGTTGACAACAAAGAAACAACAACAACAAC';
 sequence = strrep(sequence, 'T', 'U');
+% a crystallographic structure is recommended
+% untested predictions should not be used; use empty string instead
 structure = '...((((((.....))))))....(((((((....[[[[....(((((....))))).....)))))))...........(((..]]]]...)))...((((((.....)))))).........................';
-
+% data_types should reflect your experiment setup
 data_types = [ ...
     repmat({'nomod'}, 1, 8), repmat({'DMS'}, 1, 16), repmat({'CMCT'}, 1, 16), repmat({'SHAPE'}, 1, 16), ...
     repmat({'ddATP'}, 1, 2), repmat({'ddTTP'}, 1, 2), repmat({'ddCTP'}, 1, 2), repmat({'ddGTP'}, 1, 2), ...
 ];
 % offset = - length_of_5_flanking_region + N_first_nt_ROI_should_be - 1
 offset = -24;
+% this formula for first_RT_nucleotide is typically not changed
 first_RT_nucleotide = length(sequence) - 20 + offset;
 
 
-% step 4
+%% STEP 4
+% only run this line once, it clears xsel
 xsel = []; clf;
+% this line is safe to run multiple times, for updating or checkign your assignment
 [xsel, seqpos, area_pred] = annotate_sequence(d_align, xsel, sequence, offset, data_types, first_RT_nucleotide, structure, [], 2);
 
-% step 5
+%% STEP 5
 [area_peak, darea_peak] = fit_to_gaussians(d_align, xsel);
 
 
-% step 6
+%% STEP 6
 ref_segment = 'GAGUA';
+% be careful if your ROI has a GAGUA somewhere
 ref_peak = get_ref_peak(sequence, ref_segment, offset);
 sd_cutoff = 1.5;
+% pull out undiluted lanes from area_peak
 saturated_idx = [9, 10, 13, 14, 17 ,18 ,21, 22, 25, 26, 29, 30, 33, 34, 37, 38, 41, 42, 45, 46, 49, 50, 53, 54];
 
+% the mean() is because of the example has 2 nomod replicates for each condition (-/+ ligand)
+% simple cases should use saturated_array = area_peak(:, saturated_idx);
 saturated_array = [ ...
     mean(area_peak(:, 1:2), 2), ...
     mean(area_peak(:, 5:6), 2),...
     area_peak(:, saturated_idx), ...
 ];
+% in the example, indices for diluted lanes are derived from undiluted ones
 diluted_array = [ ...
     mean(area_peak(:, 3:4), 2), ...
     mean(area_peak(:, 7:8), 2),...
@@ -66,12 +84,20 @@ diluted_error = [ ...
     darea_peak(:, saturated_idx + 2), ...
 ];
 
+% the background lane (nomod) for each lane in saturated_array
+% in this example, 1 and 2 correspond to (-/+ ligand) nomods
+% the numbering is the same as saturated_array, not area_peak
 bkg_col  = [1, 2, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2];
+% make sure you have the data_types correctly
+% in this example, [1, 2, saturated_idx] prepends to 'nomod' strings to
+% reflect the (-/+ ligand) backgrounds averaged separately
+% simple cases should use data_types(saturated_idx) instead
 [normalized_reactivity, normalized_error, seqpos_out] = get_reactivities( ...
     saturated_array, diluted_array, saturated_error, diluted_error, ...
     bkg_col, ref_peak, seqpos, [], data_types([1, 2, saturated_idx]), sequence, offset, sd_cutoff);
 
-% step 7
+%% STEP 7
+% indices of normalized_reactivity should be the same as saturated_array
 [d_DMS_minus, da_DMS_minus, flags] = average_data_filter_outliers( ...
     normalized_reactivity(:, 3:4), normalized_error(:, 3:4), [], seqpos_out, sequence, offset); 
 [d_DMS_plus, da_DMS_plus, flags] = average_data_filter_outliers( ...
@@ -88,7 +114,7 @@ bkg_col  = [1, 2, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2,
     normalized_reactivity(:, 23:24), normalized_error(:, 23:24), [], seqpos_out, sequence, offset); 
 
 
-% step 8
+%% STEP 8
 labels = data_types;
 for i = 1:length(labels) - 8;
     if i <= 8;
@@ -105,15 +131,19 @@ for i = 1:length(labels) - 8;
         end;
     end;
 end;
-
-print_xsel_split(d_align, xsel(2:end), seqpos_out, sequence, offset, area_pred, labels, ...
+% you can leave out the 2nd and 3rd line optionals
+% append a 'X' to sequence allow printing the final additional band location
+print_xsel_split(d_align, xsel, seqpos_out, ['X', sequence], offset, area_pred, labels, ...
     [3 0 0 50 75 100 4 8 52 0 8 56], [0 1 0 1 1], ...
     {'RNA Puzzle #13: ZMP Aptamer', '', '', 'T47', 'May 2015'}, [25 15 9 8 11 1 1 2]);
 
 
+% this is a complicated case for plotting
+% simple cases only need commands of plot and errorbar
+% commands of make_lines, legend, title, set are decorative
 figure();
 set_print_page(gcf, 0);
-
+% do not directly copy these commands for your own dataset
 subplot(3, 1, 1);
 plot(seqpos_out, d_DMS_minus, 'b', 'linewidth', 2); hold on;
 plot(seqpos_out, d_DMS_plus, 'r', 'linewidth', 2); hold on;
@@ -162,7 +192,7 @@ make_lines([-20:20:100] - 0.5, 'k', 0.5, 0, 1);
 print_save_figure(gcf, '1D_plot_compare', './');
 
 
-% step 9
+%% STEP 9
 filename = 'pfl_1D.rdat';
 name = 'RNA Puzzle 13';
 comments = { ...
@@ -188,43 +218,56 @@ data_annotations{4} = {'modifier:DMS', 'chemical:ZMP:10uM'};
 data_annotations{5} = {'modifier:CMCT'};
 data_annotations{6} = {'modifier:CMCT', 'chemical:ZMP:10uM'};
 
+% only write ROI to RDAT, excluding flanking sequences
 rdat_idx = 25:95;
 rdat_seqpos = seqpos_out(rdat_idx);  % 1:71
 rdat_sequence = sequence(rdat_idx);
 rdat_structure = structure(rdat_idx);
 rdat_offset = 0;
 
+% put final profiles together and trim
 rdat_d_final = [d_SHAPE_minus, d_SHAPE_plus, d_DMS_minus, d_DMS_plus, d_CMCT_minus, d_CMCT_plus];
 rdat_da_final = [da_SHAPE_minus, da_SHAPE_plus, da_DMS_minus, da_DMS_plus, da_CMCT_minus, da_CMCT_plus];
-
 rdat_d_final = rdat_d_final(rdat_idx, :);
 rdat_da_final = rdat_da_final(rdat_idx, :);
 
 output_workspace_to_rdat_file(filename, name, rdat_sequence, rdat_offset, rdat_seqpos, ...
     rdat_d_final, rdat_structure, annotations, data_annotations, rdat_da_final, ...
     [], [], [], comments);
+% a preview figure of RDAT file is saved
 d_rdat = show_rdat(filename);
 
 
-% step 11
-% Prediction with no data, only thermodynamic parameters
-% No need to bootstrap since there is no data to sample
+%% STEP 11
+% prediction with no data, only thermodynamic parameters
+% no need to bootstrap since there is no data to resample
 [structure_NA, bpp_NA] = rna_structure(sequence, [], offset, seqpos_out, [], 0);
 
-% Run Fold with 100x bootstrap on 1D SHAPE data
+% run Fold with 100x bootstrap on 1D SHAPE data
+% 100x bootstrap means 100x independent resampling from the original data
+% the original (complte) data is used once, before bootstrapping
+% so total of 101 Fold calls are made
 [structure_1D_Fold_SHAPE_minus, bpp_1D_Fold_SHAPE_minus] = rna_structure(sequence, d_SHAPE_minus, offset, seqpos_out, [], 100, 0);
 [structure_1D_Fold_SHAPE_plus, bpp_1D_Fold_SHAPE_plus] = rna_structure(sequence, d_SHAPE_plus, offset, seqpos_out, [], 100, 0);
 
-% Run ShapeKnot with 100x bootstrap on 1D SHAPE data
+% run ShapeKnot with 100x bootstrap on 1D SHAPE data
+% time a single run before kicking off 100x bootstrap, it may be very long
+% use tic and toc commands, with bootstrap = 0 (see above why)
 [structure_1D_Spkt_SHAPE_minus, bpp_1D_Spkt_SHAPE_minus] = rna_structure(sequence, d_SHAPE_minus, offset, seqpos_out, [], 100, 1);
 [structure_1D_Spkt_SHAPE_plus, bpp_1D_Spkt_SHAPE_plus] = rna_structure(sequence, d_SHAPE_plus, offset, seqpos_out, [], 100, 1);
 
-% step 12
+%% STEP 12
 output_varna_html('pfl_NA.html', sequence, structure_NA, structure, structure_NA, offset, [], [], [], bpp_NA);
 
-output_varna_html('pfl_1D_Fold_SHAPE_minus.html', sequence, structure_1D_Fold_SHAPE_minus, structure, structure_1D_Fold_SHAPE_minus, offset, [], [], [d_SHAPE_minus; zeros(20, 1)], bpp_1D_Fold_SHAPE_minus);
-output_varna_html('pfl_1D_Spkt_SHAPE_minus.html', sequence, structure_1D_Spkt_SHAPE_minus, structure, structure_1D_Spkt_SHAPE_minus, offset, [], [], [d_SHAPE_minus; zeros(20, 1)], bpp_1D_Spkt_SHAPE_minus);
+% the 3 structure-related arguments (#3-5) are
+% display_structure: RNA secstr for display (drawing backbone in)
+% native_structure: reference/native secstr used for secstr comparison.
+% modeled_structure: predicted/new secstr used for secstr comparison.
+output_varna_html('pfl_1D_Fold_SHAPE_minus.html', sequence, structure_1D_Fold_SHAPE_minus, structure, structure_1D_Fold_SHAPE_minus, offset, [], [], [d_SHAPE_minus; nan(20, 1)], bpp_1D_Fold_SHAPE_minus);
+output_varna_html('pfl_1D_Spkt_SHAPE_minus.html', sequence, structure_1D_Spkt_SHAPE_minus, structure, structure_1D_Spkt_SHAPE_minus, offset, [], [], [d_SHAPE_minus; nan(20, 1)], bpp_1D_Spkt_SHAPE_minus);
 
-output_varna_html('pfl_1D_Fold_SHAPE_plus.html', sequence, structure_1D_Fold_SHAPE_plus, structure, structure_1D_Fold_SHAPE_plus, offset, [], [], [d_SHAPE_plus; zeros(20, 1)], bpp_1D_Fold_SHAPE_plus);
-output_varna_html('pfl_1D_Spkt_SHAPE_plus.html', sequence, structure_1D_Spkt_SHAPE_plus, structure, structure_1D_Spkt_SHAPE_plus, offset, [], [], [d_SHAPE_plus; zeros(20, 1)], bpp_1D_Spkt_SHAPE_plus);
+% the reactivity vector should be the same length with sequence and structure
+% thus 20 of NaN are appended and it will be displayed as gray
+output_varna_html('pfl_1D_Fold_SHAPE_plus.html', sequence, structure_1D_Fold_SHAPE_plus, structure, structure_1D_Fold_SHAPE_plus, offset, [], [], [d_SHAPE_plus; nan(20, 1)], bpp_1D_Fold_SHAPE_plus);
+output_varna_html('pfl_1D_Spkt_SHAPE_plus.html', sequence, structure_1D_Spkt_SHAPE_plus, structure, structure_1D_Spkt_SHAPE_plus, offset, [], [], [d_SHAPE_plus; nan(20, 1)], bpp_1D_Spkt_SHAPE_plus);
 
--- Examples/example_pfl_2D.m
+++ Examples/example_pfl_2D.m
@@ -1,38 +1,43 @@
-% step 1
+%% STEP 1
 filenames = {'pfl_2D_SHAPE_plus_409193'};
 [d_align, d_ref_align, ylimit, labels] = quick_look(filenames, [], 1:72);
 
-% step 2
+%% STEP 2
+% excludes the bad lane #56
 align_blocks = {[1:55, 57:72]};
 d_align_before_more_alignment = d_align;
 d_align_dp_fine = align_by_DP_fine(d_align_before_more_alignment, align_blocks);
 d_align = d_align_dp_fine;
 
 
-% step 3
+%% STEP 3
 sequence = 'GGAGACCTCGAGTAGAGGTCAAAAGGGTCGTGACTGGCGAACAGGTGGGAAACCACCGGGGAGCGACCCCGGCATCGATAGCCGCCCGCCTGGGCAAACAACTCGAGTAGAGTTGACAACAAAGAAACAACAACAACAAC';
 sequence = strrep(sequence, 'T', 'U');
+% use crystallographic struture for guidance
+% untested ones should not be used here
 structure = '...((((((.....))))))....(((((((....[[[[....(((((....))))).....)))))))...........(((..]]]]...)))...((((((.....)))))).........................';
 offset = -24;
 first_RT_nucleotide = length(sequence) - 20 + offset; 
 
+% the first lane is WT, thus NaN in terms of mutation
 data_types{1} = 'NaN';
 for i = 2:72;
     data_types{i} = [num2str(i - 1)];
 end;
 
 
-% step 4
+%% STEP 4
 xsel = []; clf;
+% remove the extra final band
 [xsel, seqpos, area_pred] = annotate_sequence(d_align, xsel, sequence, offset, data_types, first_RT_nucleotide, structure);
 
-% step 5
+%% STEP 5
 [area_peak, darea_peak] = fit_to_gaussians(d_align, xsel);
 area_peak(:, 56) = 0;
 darea_peak(:, 56) = 0;
 
 
-% step 9
+%% STEP 9
 filename = 'pfl_2D_SHAPE_plus.rdat';
 name = 'RNA Puzzle 13';
 comments = { ...
@@ -50,10 +55,12 @@ annotations = { ...
     'modifier:SHAPE', ...
 };
 
+% loop generates a cell of mutation labels, e.g. U55A
 data_annotations{1} = 'mutation:WT';
 for j = 2:size(area_peak, 2); 
     data_annotations{j} = {['mutation:', sequence(j - 1 - offset), num2str(j - 1), DNA2RNA(complement(sequence(j - 1 - offset)))]};
 end;
+% manually mark bad lane
 data_annotations{56} = {'mutation:U55A', 'warning:badQuality'};
 
 output_workspace_to_rdat_file(filename, name, sequence, offset, seqpos, area_peak, ...
@@ -61,16 +68,18 @@ output_workspace_to_rdat_file(filename, name, sequence, offset, seqpos, area_pea
 d_rdat = show_rdat(filename);
 
 
-% step 8
+%% STEP 8
 d_align(:, 56) = 0;
+% you need the RDAT instance for printing
 print_CE_split(d_align, d_rdat, [], [], xsel, [], [], area_pred, ...
     [2 2], [1 0 1 1 1 1 1 0 1 1 1], ...
     {'', 'print_CE_split_output', 'SHAPE', 'T47', 'May 2015'});
 
 
-% step 10
+%% STEP 10
 Z = output_Zscore_from_rdat('pfl_SHAPE_2Dbonus.txt', {filename});
 
+% filtering of Z score by sequence positions
 Z_cutoff_mean = Z;
 for i = 1:size(Z_cutoff_mean, 1);
     Z_cutoff_mean(i, Z_cutoff_mean(i, :) >= mean(Z_cutoff_mean(i, :))) = 0;
@@ -82,13 +91,13 @@ for i = 1:size(Z_cutoff_1std, 1);
 end;
 
 
-% step 11
-% Run Fold with 100x bootstrap on 2D SHAPE data
+%% STEP 11
+% run Fold with 100x bootstrap on 2D SHAPE data
 [structure_2D_Fold_SHAPE, bpp_2D_Fold_SHAPE] = rna_structure(sequence, [], offset, seqpos, Z, 100, 0);
-% Run ShapeKnot with 100x bootstrap on 2D SHAPE data
+% run ShapeKnot with 100x bootstrap on 2D SHAPE data
 [structure_2D_Spkt_SHAPE, bpp_2D_Spkt_SHAPE] = rna_structure(sequence, [], offset, seqpos, Z, 100, 1);
 
-% step 12
+%% STEP 12
 output_varna_html('pfl_2D_Fold_SHAPE.html', sequence, structure_2D_Fold_SHAPE, structure, structure_2D_Fold_SHAPE, offset, [], [], [], bpp_2D_Fold_SHAPE);
 output_varna_html('pfl_2D_Spkt_SHAPE.html', sequence, structure_2D_Spkt_SHAPE, structure, structure_2D_Spkt_SHAPE, offset, [], [], [], bpp_2D_Spkt_SHAPE);
 
