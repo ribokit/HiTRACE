a9664f6c88b481818d875515dc0defbfd237e27a
Rhiju Das <rhiju@stanford.edu>
2017-06-19 17:07:26 -0700

    updated split_string. Will use strsplit if your version of MATLAB has it; otherwise, there is a fallback

--- Scripts/utility/string/split_string.m
+++ Scripts/utility/string/split_string.m
@@ -1,28 +1,27 @@
-function cols = split_string( l, delimiter );
-
+function cols = split_string( input_string, delimiter );
+% cols = split_string( input_string, delimiter );
+%
+% INPUT:
+%   input_string = input string
+%   delimiter    = character for delimiting (\t is OK for tabs). Default: ' '.
+%
+% OUTPUT:
+%   cols      = cell of strings that the input string was split into
+%
+if nargin < 1; help( mfilename); cols = {}; return; end;
 if ~exist('delimiter', 'var'); delimiter = ' '; end;
+if exist( 'strsplit', 'file')
+    % newer versions of MATLAB have strsplit
+    cols = strsplit( input_string, delimiter ); return;
+end
+
 delimiter = sprintf(delimiter);
 
-remain = l;
+remain = input_string;
 cols = {};
 while length( remain ) > 0
   [token, remain] = strtok(remain, delimiter);
   cols = [cols, token];
 end
 
-
-%in_space = 1;
-%count = 0;
-%for i = [1:length(l)]
-%  if ( strcmp( l(i), delimiter ) )
-%    in_space = 1;
-%  else 
-%    if ( in_space )
-%      count = count + 1;
-%      cols{ count }  = '';
-%      in_space = 0;
-%    end
-%    cols{count} = [cols{count}, l(i)];
-%  end
-%end
 
